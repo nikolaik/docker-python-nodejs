48c2199ce6cff654776808b67d8d02c5381b6a52
Nikolai R Kristiansen <nikolaik@gmail.com>
2022-01-09 16:38:50 +0100

    🔥 Remove scheduled config [skip ci]
    ➖ Remove unused docker-py dep

--- build_versions/ci_config.py
+++ build_versions/ci_config.py
@@ -5,6 +5,7 @@ import yaml
 
 from build_versions.settings import CONFIG_GENERATED_PATH, CONFIG_TEMPLATE_PATH
 
+WORKFLOW_NAME = "build"
 DEPLOY_JOB_NAME = "deploy"
 RELEASE_JOB_NAME = "release"
 
@@ -18,33 +19,30 @@ def generate_config(new_or_updated):
     with open(CONFIG_TEMPLATE_PATH) as fp:
         config = yaml.load(fp, Loader=yaml.FullLoader)
 
-    # Update config template workflows with per version jobs
-    for name, workflow in config["workflows"].items():
-        if name == "version":
-            continue
+    # Update config template workflow with per version jobs
+    # Add deploy jobs for each new or updated version based on deploy job in template...
+    # meaning if there are noe changes, there's no deploy.
+    jobs = []
+    version_jobs_names = []
+    workflow = config["workflows"][WORKFLOW_NAME]
+    for job in workflow["jobs"]:
+        if isinstance(job, dict) and DEPLOY_JOB_NAME in job:
+            for version in new_or_updated:
+                version_job = copy.deepcopy(job)
+                job_name = f"deploy_{version['key']}"
+                version_job[DEPLOY_JOB_NAME]["name"] = job_name
+                version_job[DEPLOY_JOB_NAME]["version_key"] = version["key"]
+                version_job[DEPLOY_JOB_NAME]["version_config"] = json.dumps(version)
+                version_jobs_names.append(job_name)
+                jobs.append(version_job)
+        elif isinstance(job, dict) and RELEASE_JOB_NAME in job and version_jobs_names:
+            # Make release job depend on all others
+            job[RELEASE_JOB_NAME]["requires"] = version_jobs_names
+            jobs.append(job)
+        else:
+            jobs.append(job)
 
-        # pop existing deploy and git archive job and add new ones based on the existing.
-        # Meaning if there is nothing new, there is no deploy.
-        jobs = []
-        version_jobs_names = []
-        for job in workflow["jobs"]:
-            if isinstance(job, dict) and DEPLOY_JOB_NAME in job:
-                for version in new_or_updated:
-                    version_job = copy.deepcopy(job)
-                    job_name = f"deploy_{version['key']}"
-                    version_job[DEPLOY_JOB_NAME]["name"] = job_name
-                    version_job[DEPLOY_JOB_NAME]["version_key"] = version["key"]
-                    version_job[DEPLOY_JOB_NAME]["version_config"] = json.dumps(version)
-                    version_jobs_names.append(job_name)
-                    jobs.append(version_job)
-            elif isinstance(job, dict) and RELEASE_JOB_NAME in job:
-                # Make git archive job depend on all others
-                job[RELEASE_JOB_NAME]["requires"] = version_jobs_names
-                jobs.append(job)
-            else:
-                jobs.append(job)
-
-        config["workflows"][name] = workflow | {"jobs": jobs}
+    config["workflows"][WORKFLOW_NAME] = workflow | {"jobs": jobs}
 
     # Write generated CI config
     with open(CONFIG_GENERATED_PATH, "w+") as fp_out:
