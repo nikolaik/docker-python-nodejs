35c674a569d17558cb877698c1161a616237fbf6
Nikolai Røed Kristiansen <nikolai.kristiansen@remarkable.no>
2023-06-04 17:04:21 +0200

    🏷️ Type dicts using dataclasses

--- build_versions/readme.py
+++ build_versions/readme.py
@@ -4,18 +4,18 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from build_versions.versions import LanguageVersion
+    from build_versions.versions import BuildVersion, SupportedVersion
 
 logger = logging.getLogger("dpn")
 
 
-def _format_md_table(columns: list[str], rows: list[list[str]]):
+def _format_md_table(columns: list[str], rows: list[list[str]]) -> str:
     head = f"{' | '.join(columns)}\n{' | '.join(['---'] * len(columns))}"
     body = "\n".join([" | ".join(row) for row in rows])
     return f"{head}\n{body}\n"
 
 
-def _replace(name: str, replacement: str, document: str):
+def _replace(name: str, replacement: str, document: str) -> str:
     start = f"<!-- {name}_START -->\n"
     end = f"\n<!-- {name}_END -->"
     repl = f"{start}\n{replacement}\n{end}"
@@ -23,11 +23,11 @@ def _replace(name: str, replacement: str, document: str):
 
 
 def update_dynamic_readme(
-    versions,
-    python_versions: "list[LanguageVersion]",
-    node_versions: "list[LanguageVersion]",
+    versions: "list[BuildVersion]",
+    python_versions: "list[SupportedVersion]",
+    node_versions: "list[SupportedVersion]",
     dry_run=False,
-):
+) -> None:
     """Read out current README, format fresh README, write back possible changes"""
     readme_path = Path("README.md")
     with readme_path.open() as fp:
@@ -46,11 +46,11 @@ def update_dynamic_readme(
 
 
 def format_readme(
-    versions,
-    python_versions: "list[LanguageVersion]",
-    node_versions: "list[LanguageVersion]",
+    versions: "list[BuildVersion]",
+    python_versions: "list[SupportedVersion]",
+    node_versions: "list[SupportedVersion]",
     readme: str,
-):
+) -> str:
     """Format fresh README based on passed in version. Replaces the whole table with new versions."""
     tags_table = format_tags(versions)
     readme_fresh = _replace("TAGS", tags_table, readme)
@@ -59,16 +59,16 @@ def format_readme(
     return _replace("SUPPORTED_VERSIONS", supported_versions_table, readme_fresh)
 
 
-def format_tags(versions):
+def format_tags(versions: "list[BuildVersion]") -> str:
     headings = ["Tag", "Python version", "Node.js version", "Distro", "Platforms"]
-    rows = [
-        [f"`{v['key']}`", v["python_canonical"], v["nodejs_canonical"], v["distro"], ", ".join(v["platforms"])]
-        for v in versions
-    ]
+    rows = [[f"`{v.key}`", v.python_canonical, v.nodejs_canonical, v.distro, ", ".join(v.platforms)] for v in versions]
     return _format_md_table(headings, rows)
 
 
-def format_supported_versions(python_versions: "list[LanguageVersion]", node_versions: "list[LanguageVersion]") -> str:
+def format_supported_versions(
+    python_versions: "list[SupportedVersion]",
+    node_versions: "list[SupportedVersion]",
+) -> str:
     headings = ["Python version", "Start", "End"]
     rows = [[ver.version, ver.start, ver.end] for ver in sorted(python_versions, key=lambda x: x.start, reverse=True)]
     python_table = _format_md_table(headings, rows)
