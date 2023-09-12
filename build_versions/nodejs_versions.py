0e2972526fe9d1699c71b80441c9d34a6015b2cf
Nikolai Røed Kristiansen <nikolai.kristiansen@remarkable.no>
2023-09-12 21:22:58 +0200

    ✨ Check nodejs release platform availability
    Nodejs releases must be built for platform/architecture (x64/arm64/musl).
    We previously did a simple check for the proper nodejs tag on docker hub, but since we now use the offically distributed binaries, we instead check the index.json which contains this info.

--- build_versions/nodejs_versions.py
+++ build_versions/nodejs_versions.py
@@ -1,8 +1,8 @@
+from collections.abc import Mapping
 from datetime import datetime
+from typing import TypedDict
 
 import requests
-
-from build_versions.versions import SupportedVersion
 
 todays_date = datetime.utcnow().date().isoformat()
 
@@ -15,14 +15,40 @@ def fetch_node_gpg_keys() -> list[str]:
     return res.text.strip().split("\n")
 
 
-def fetch_supported_nodejs_versions() -> list[SupportedVersion]:
+class NodeRelease(TypedDict):
+    version: str
+    date: str
+    files: list[str]
+
+
+def fetch_node_releases() -> list[NodeRelease]:
+    """Fetch offical node releases"""
+    url = "https://nodejs.org/dist/index.json"
+    res = requests.get(url, timeout=10.0)
+    res.raise_for_status()
+    data: list[NodeRelease] = res.json()
+    return data
+
+
+def fetch_node_unofficial_releases() -> list[NodeRelease]:
+    url = "https://unofficial-builds.nodejs.org/download/release/index.json"
+    res = requests.get(url, timeout=10.0)
+    res.raise_for_status()
+    data: list[NodeRelease] = res.json()
+    return data
+
+
+class ReleaseScheduleItem(TypedDict):
+    start: str
+    lts: str
+    maintenance: str
+    end: str
+    codename: str
+
+
+def fetch_nodejs_release_schedule() -> Mapping[str, ReleaseScheduleItem]:
     """Download list of official releases, skipping unreleased and unsupported versions"""
-    result = requests.get("https://raw.githubusercontent.com/nodejs/Release/master/schedule.json", timeout=10.0)
-    release_schedule = result.json()
-
-    versions = []
-    for ver, detail in release_schedule.items():
-        if detail["start"] <= todays_date <= detail["end"]:
-            versions.append(SupportedVersion(version=ver, start=detail["start"], end=detail["end"]))
-
-    return versions
+    res = requests.get("https://raw.githubusercontent.com/nodejs/Release/master/schedule.json", timeout=10.0)
+    res.raise_for_status()
+    release_schedule: Mapping[str, ReleaseScheduleItem] = res.json()
+    return release_schedule
